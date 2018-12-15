3985855419ec3bc213c43db18aa6674ba8244eb3
Sabina Pashayeva <sabipash270599@gmail.com>
2018-12-16 01:43:45 +0200

    Add parser

---
+++ IndividualTask1/Parser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace IndividualTask1
+{
+    public static class Parser
+    {
+        static readonly Dictionary<Regex, Func<string, IExpression>> typeList;
+        static readonly Regex staticMethodRegex = new Regex("[a-zA-Z].*=>.+");
+        static readonly Regex variableAssignRegex = new Regex("[a-zA-Z].*<=.+");
+
+        static Parser()
+        {
+            typeList = new Dictionary<Regex, Func<string, IExpression>>
+            {
+                { staticMethodRegex, (str) => new StaticMethodModel(str) },
+                { variableAssignRegex, (str) => new VariableAssignModel(str) }
+            };
+        }
+
+        public static IExpression GetExpression(string command)
+        {
+            foreach (var pattern in typeList.Keys)
+            {
+                if (!pattern.IsMatch(command))
+                    continue;
+
+                return typeList[pattern](command);
+            }
+
+            return default(IExpression);
+        }
+    }
+}
